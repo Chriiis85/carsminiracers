0b30114ba37277d8437c3fa20b2c39087fe7ff94
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-09-23 16:55:30 +0200

    Landing Page primera version y Puertas funcionando hacia fuera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DoorsComponent } from "./doors/doors.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DoorsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'carsminiracers';
+  //Funcionamiento mostrar/ocultar constraseña
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  redirectInstagram(){
+    window.open("https://www.instagram.com/carsminiracers.es/", "_blank");
+  }
 }
